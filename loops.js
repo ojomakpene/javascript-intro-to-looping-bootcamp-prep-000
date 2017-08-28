3717af9c8e8da18dfc8d3f9840ad191c8a4d591e
Sani Abubakar <egbaojo@gmail.com>
2017-08-28 09:07:53 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array) {
+const j = []
+for (var i = 0; i <25; i++) {
+if (i ===1) {
+  j.push('I am 1 strange loop.')
+}
+else {
+  j.push(`I am ${i} strange loops.`)
+}
+}
+return console.log(j)
+}
